062f5931d81050fb60bbad9eec98b5e0820ddf89
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2025-06-04 23:20:16 +0900

    add cluster

---
+++ docker/init-cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Waiting for Redis nodes to start..."
+sleep 5
+
+echo "Creating cluster..."
+yes yes | redis-cli --cluster create \
+  redis-node-1:6379 \
+  redis-node-2:6379 \
+  redis-node-3:6379 \
+  redis-node-4:6379 \
+  redis-node-5:6379 \
+  redis-node-6:6379 \
+  --cluster-replicas 1
+
+echo "Cluster creation finished."
