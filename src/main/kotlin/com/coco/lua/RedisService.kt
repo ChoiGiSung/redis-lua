ee2161c862a5dfbaf1d0797075fb145c029af04b
DESKTOP-MJ25L1Q\chlrl <gisungcu@gmail.com>
2025-06-08 16:43:40 +0900

    role 확인 중

--- src/main/kotlin/com/coco/lua/RedisService.kt
+++ src/main/kotlin/com/coco/lua/RedisService.kt
@@ -1,5 +1,6 @@
 package com.coco.lua
 
+import org.springframework.data.redis.connection.RedisConnection
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.core.script.DefaultRedisScript
 import org.springframework.stereotype.Service
@@ -10,11 +11,13 @@ class RedisService(
 
 ) {
     fun get(key: String): String {
-        return redisTemplate.opsForValue().get(key).toString()
+//        return redisTemplate.opsForValue().get(key).toString()
+        return readData(key) ?: ""
     }
 
     fun set(key: String, value: String) {
-        redisTemplate.opsForValue().set(key, value)
+//        redisTemplate.opsForValue().set(key, value)
+        saveData(key, value)
     }
 
     fun decrease(key: String, quantity: Int): Long? {
@@ -29,6 +32,25 @@ class RedisService(
         )
         return result
     }
+
+    fun getNodeRoleForAKey(key: String?): String? {
+        return redisTemplate.execute({ connection: RedisConnection ->
+            val infoReplication = connection.serverCommands().info("replication") // "replication" 섹션 지정
+            infoReplication?.getProperty("role") ?: "Unknown" // role 속성 추출
+        }, true) // true는 pipeline/transaction을 사용하지 않음을 의미
+    }
+
+    fun saveData(key: String?, value: String?) {
+        redisTemplate.opsForValue()[key!!] = value!!
+        println("Data saved on node: " + getNodeRoleForAKey(key))
+    }
+
+    fun readData(key: String): String? {
+        val data = redisTemplate.opsForValue()[key]?.toString() ?: ""
+        println("Data read from node: " + getNodeRoleForAKey(key))
+        return data
+    }
+
 
     companion object {
         // Lua 스크립트: 주어진 키의 값을 ARGV[1] 만큼 감소시키고, 감소된 값을 반환합니다.
